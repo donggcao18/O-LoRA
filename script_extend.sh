04c1812d80915881e737b47098ce3040801e7e97
donggcao18 <caovdongg18@gmail.com>
2026-03-29 02:16:38 +0700

    +

--- script_extend.sh
+++ script_extend.sh
@@ -26,9 +26,9 @@ python src/run_uie_lora.py  \
     --logging_strategy steps \
     --logging_steps 25  \
     --evaluation_strategy steps \
-    --eval_steps 50  \
+    --eval_steps 10  \
     --save_strategy steps \
-    --save_steps 50  \
+    --save_steps 10  \
     --save_total_limit 2  \
     --fp16 \
     --report_to none
